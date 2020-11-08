1e7524114820171f4f53bc85f2b489fe4f60d294
brittchin99 <bchin@colgate.edu>
2020-11-08 20:24:23 +0000

    update to log in and create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+    helper_method :resource_name, :resource, :devise_mapping, :resource_class
+    def resource_name
+      :account
+    end
+     
+    def resource
+      @resource ||= Account.new
+    end
+    def resource_class
+      Account
+    end
+     
+    def devise_mapping
+      @devise_mapping ||= Devise.mappings[:account]
+    end
 end
