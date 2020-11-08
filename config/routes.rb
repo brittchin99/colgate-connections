551d2ed2f17b12dda165111a333bb51ff484b6c7
Si Wei <swei@colgate.edu>
2020-11-08 21:31:45 +0000

    Add ProfilesController

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :accounts, :controllers => {:registrations => "accounts"} 
     resources :accounts, :only => [:index, :show, :new, :create]
+    resources :profiles, :only => [:index, :show]
     resources :connections, :only => [:index, :show, :new, :create]
     
     root :to => "connections#index"
