a36ae92a32ba40398c9b273d041356a3b94cb425
ntp99 <ntp99x@gmail.com>
2020-11-09 20:34:50 +0000

    Added Colgate email validation

--- app/models/account.rb
+++ app/models/account.rb
@@ -22,7 +22,9 @@ class Account < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   validates :first_name, :last_name, :email, :password, :pronouns, :class_year, :majors, :interests, presence: true
-  validates :email, uniqueness: true
+  validates :email, format: { with: /\A(.+)@colgate\.edu\z/i, message: "must contain @colgate.edu"},
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 6, maximum: 254 }      
 
   PRONOUNS = ['she/her/hers', 'he/him/his', 'they/them/theirs', 'Other']
   MAJORS = ["Africana and Latin American Studies","Anthropology","Applied Mathematics","Art and Art History","Asian Studies","Astrogeophysics","Astronomy/Physics","Biochemistry","Biology","Chemistry","Chinese","Classical Studies","Computer Science","Computer Science/Mathematics","Creative Writing","Economics","Educational Studies","English","Environmental Biology","Environmental Economics","Environmental Geography","Environmental Geology","Environmental Studies","Film and Media Studies","French","Geography","Geology","German","Greek","History","International Relations","Japanese","Jewish Studies","Latin","LGBTQ Studies","Linguistics","Mathematical Economics","Mathematical Systems Biology","Mathematics","Medieval and Renaissance Studies","Middle Eastern and Islamic Studies","Molecular Biology","Museum Studies","Music","Native American Studies","Neuroscience","Peace and Conflict Studies","Philosophy","Philosophy and Religion","Physical Science","Physics","Political Science","Psychological Science","Religion","Russian and Eurasian Studies","Sociology","Spanish","Theater","Women's Studies","Writing and Rhetoric"]
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-
-  get 'homes/index'
   devise_for :accounts, :controllers => {:registrations => "accounts"} 
-  resources :accounts, :only => [:index, :show, :new, :create]
+  resources :accounts, :only => [:index, :new, :create]
   resources :profiles, :only => [:index, :show]
   resources :connections, :only => [:index, :show, :new, :create]
   resources :homes, :only => [:index]
