f07c57102ded2054e453fc9bd1b91e7aa2159797
ntp99 <ntp99x@gmail.com>
2020-11-09 04:24:52 +0000

    No fake friends

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -2,6 +2,8 @@ class Connection < ApplicationRecord
     belongs_to :account, class_name: "Account"
     belongs_to :friend, class_name: "Account"
     
+    validates :account, uniqueness: {scope: :friend, message: "You can only add a friend once"}
+    
     def create
     end
 
