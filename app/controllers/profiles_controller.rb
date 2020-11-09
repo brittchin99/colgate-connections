1c2557d2a953568ab4dd2122c761605b8ebeb01c
Ubuntu <ubuntu@ip-172-31-32-68.ec2.internal>
2020-11-09 07:29:52 +0000

    add filtering and searching for profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,4 +2,29 @@ class ProfilesController < ApplicationController
   def show
     @profile = Account.find(params[:id])
   end
+  
+  def index
+    @profiles = Account.all
+    if params.has_key?("/profiles")
+      # THE FORM IN INDEX SHOULD BE FIXED
+      p = params["/profiles"]
+      @profiles = @profiles.where("first_name LIKE ?", "%#{p["first_name"]}")
+                          .where("last_name LIKE ?", "%#{p["last_name"]}")
+                          .where("email LIKE ?", "%#{p["email"]}")
+                          .where("pronouns LIKE ?", "%#{p["pronouns"]}")
+                          .where("class_year LIKE ?", "%#{p["class_year"]}")
+                          .where("majors LIKE ?", "%#{p["majors"]}")
+                          .where("minors LIKE ?", "%#{p["minors"]}")
+                          .where("interests LIKE ?", "%#{p["interests"]}")
+    elsif params.has_key?("query")
+      @profiles = @profiles.where("first_name LIKE ? 
+                                  OR last_name LIKE ?
+                                  OR email LIKE ?
+                                  OR pronouns LIKE ? 
+                                  OR class_year LIKE ?
+                                  OR majors LIKE ?
+                                  OR minors LIKE ?
+                                  OR interests LIKE ?", params["query"], params["query"], params["query"], params["query"], params["query"], params["query"], params["query"], params["query"])
+    end
+  end
 end
