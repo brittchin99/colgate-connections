f0d9df6a05982061c0e852cefd710373a65e91f9
Si Wei <swei@colgate.edu>
2020-11-09 01:03:22 +0000

    Add Edit profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,4 +2,22 @@ class ProfilesController < ApplicationController
   def show
     @profile = Account.find(params[:id])
   end
+  
+  def edit
+      @profile = Account.find(params[:id])
+  end
+
+  def update 
+    @profile = Account.find(params[:id])
+    if @profile.update(profile_params)
+      redirect_to profile_path(@profile)
+    else 
+      render json: @profile.errors.full_messages, status: 422
+    end 
+  end 
+    
+  private
+  def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :email, :password, :pronouns, :class_year, :majors => [], :minors => [], :interests => [])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :accounts, :controllers => {:registrations => "accounts"} 
     resources :accounts, :only => [:index, :show, :new, :create]
-    resources :profiles, :only => [:index, :show]
+    resources :profiles, :only => [:index, :show, :update, :edit]
     resources :connections, :only => [:index, :show, :new, :create]
     
     root :to => "connections#index"
