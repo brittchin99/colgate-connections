e175491d9041d86cd0a9e53183204b2d60187116
ntp99 <ntp99x@gmail.com>
2020-11-10 02:45:11 +0000

    Make exclusive editing choices for account owner

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,13 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_account!
+  def index
+    @connections = current_account.friendships
+  end
+  
+  def index
+    @connections = current_account.friendships
+  end
+  
   def show
     @profile = Account.find(params[:id])
   end
@@ -18,30 +26,34 @@ class ProfilesController < ApplicationController
   end 
 
   def index
+    @connections = current_account.friendships
+    
     @profiles = Account.all
-    
-    if !params.has_key?("reset")
-      if p = (params["filter_list"] || session["filter_list"])
-        if !p.has_key?("general_search_term")
-          @profiles = @profiles.where("first_name LIKE ?", "%#{p["first_name"]}")
-                              .where("last_name LIKE ?", "%#{p["last_name"]}")
-                              .where("email LIKE ?", "%#{p["email"]}")
-                              .where("pronouns LIKE ?", "%#{p["pronouns"]}")
-                              .where("class_year LIKE ?", "%#{p["class_year"]}")
-                              .where("majors LIKE ?", "%#{p["majors"]}")
-                              .where("minors LIKE ?", "%#{p["minors"]}")
-                              .where("interests LIKE ?", "%#{p["interests"]}")
-        else
-          @profiles = @profiles.where("first_name LIKE ? 
-                                      OR last_name LIKE ?
-                                      OR email LIKE ?
-                                      OR pronouns LIKE ? 
-                                      OR class_year LIKE ?", p["general_search_term"], p["general_search_term"], p["general_search_term"], p["general_search_term"], p["general_search_term"])
-        end
-        session["filter_list"] = params["filter_list"]
-      end
+    if params.has_key?("/profiles")
+      # THE FORM IN INDEX SHOULD BE FIXED
+      p = params["/profiles"]
+      @profiles = @profiles.where("first_name LIKE ?", "%#{p["first_name"]}")
+                          .where("last_name LIKE ?", "%#{p["last_name"]}")
+                          .where("email LIKE ?", "%#{p["email"]}")
+                          .where("pronouns LIKE ?", "%#{p["pronouns"]}")
+                          .where("class_year LIKE ?", "%#{p["class_year"]}")
+                          .where("majors LIKE ?", "%#{p["majors"]}")
+                          .where("minors LIKE ?", "%#{p["minors"]}")
+                          .where("interests LIKE ?", "%#{p["interests"]}")
+    elsif params.has_key?("query")
+      @profiles = @profiles.where("first_name LIKE ? 
+                                  OR last_name LIKE ?
+                                  OR email LIKE ?
+                                  OR pronouns LIKE ? 
+                                  OR class_year LIKE ?
+                                  OR majors LIKE ?
+                                  OR minors LIKE ?
+                                  OR interests LIKE ?", params["query"], params["query"], params["query"], params["query"], params["query"], params["query"], params["query"], params["query"])
     end
-    
   end
- 
+  
+  private
+  def profile_params
+    params.require(:account).permit(:first_name, :last_name, :email, :password, :pronouns, :class_year, :majors => [], :minors => [], :interests => [])
+  end
 end
