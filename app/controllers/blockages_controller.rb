1e806360fdd13cc152559cd2de54ffe0f91b2926
ntp99 <ntp99x@gmail.com>
2020-12-05 22:04:10 +0000

    Restrict messaging to only connections

--- app/controllers/blockages_controller.rb
+++ app/controllers/blockages_controller.rb
@@ -1,4 +1,7 @@
 class BlockagesController < ApplicationController
+  before_action :authenticate_account!
+  before_action :populate_info!
+    
   def index
     @blockages = current_account.profile.blockees
   end
@@ -6,6 +9,18 @@ class BlockagesController < ApplicationController
   def create
     @blockee = Profile.find(params[:blockee_id])
     @blockage = current_account.profile.blockages.build(:blockee_id => params[:blockee_id])
+    @connection1 = current_account.profile.connections.find_by(friend_id: params[:blockee_id])
+    @friend_request1 = current_account.profile.friend_requests.find_by(friend_id: params[:friend_id])
+    @friend_request2 = @blockee.friend_requests.find_by(friend_id: current_account.profile.id)
+    if @connection1
+      @connection2 = @blockee.connections.find_by(friend_id: current_account.profile.id)
+      @connection1.destroy
+      @connection2.destroy
+    elsif @friend_request1
+      @friend_request1.destroy
+    elsif @friend_request2
+      @friend_request2.destroy
+    end
     if @blockage.save
       # flash[:notice] = "User blocked."
     else
