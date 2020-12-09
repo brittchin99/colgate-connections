0ab28e589aa58dac70f48b1b5b1753ab6fb03ad8
brittchin99 <bchin@colgate.edu>
2020-12-09 03:19:21 +0000

    connecting form to settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -6,4 +6,13 @@ class SettingsController < ApplicationController
         @profile = Profile.find(params[:id])
         @setting = @profile.setting
     end
+    
+    def update
+        puts setting_params
+    end
+    
+    private
+    def setting_params
+        params.require(:setting).permit(:dating, :preferences, :public => [], :notifs => [])
+    end 
 end
--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,4 +1,7 @@
 class Setting < ApplicationRecord
     belongs_to :profile
     
+    def to_list(s)
+        s.tr('[]', '').tr('"', '').split(',').map(&:strip)
+    end
 end
