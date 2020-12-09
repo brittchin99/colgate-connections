2bdc2f2622cc706e8271d0a1dd17046df9f80b36
ntp99 <ntp99x@gmail.com>
2020-12-09 07:48:08 +0000

    fixed suggestions

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -5,7 +5,7 @@ class ConnectionsController < ApplicationController
     def index
         @friend_requests = current_account.profile.friend_requests
         @connections = current_account.profile.connections.joins(:friend)
-        @suggestions = current_account.profile.suggested_connections.where("id NOT IN (?)", current_account.profile.connections.map(&:id).join(','))
+        @suggestions = current_account.profile.suggested_connections
         
         @sort_key = "Name"
         if !params.has_key?("reset")
