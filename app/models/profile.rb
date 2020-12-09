6e9eff67facd81cec5884343a9caf22857e4e5c5
brittchin99 <bchin@colgate.edu>
2020-12-09 19:42:45 +0000

    adding conditionals for settings

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -34,6 +34,25 @@ class Profile < ApplicationRecord
     end
   end
   
+  def is_a_match(profile)
+    if self.setting.dating && profile.setting.dating && profiles_match(self, profile)
+      true
+    else
+      false
+    end
+  end
+  
+  def profiles_match(current_account, profile)
+    if Setting.to_list(profile.setting.preference.pronouns).include? current_account.pronouns and
+      Setting.to_list(current_account.setting.preference.pronouns).include? profile.pronouns and
+      Setting.to_list(current_account.setting.preference.class_years).include? profile.class_year.to_s and
+      Setting.to_list(profile.setting.preference.class_years).include? current_account.class_year.to_s
+      true
+    else
+      false
+    end
+  end
+  
   def get_mutual_connections(profile)
     Profile.where("id IN (SELECT a.friend_id FROM Connections a, Connections b WHERE a.friend_id = b.friend_id AND a.profile_id = ? AND b.profile_id = ?)", self.id, profile.id)
   end
